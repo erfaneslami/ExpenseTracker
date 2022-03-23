2de8fb3b51d99412b8f62f048ada47d467c7987c
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-23 15:19:14 +0430

    add expenseItem style

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ExpenseItem></ExpenseItem>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
