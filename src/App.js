ca59d790a52b6eb3416d06419f89824c704e6be3
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-23 21:24:08 +0430

    split date component into it own component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import "./App.scss";
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
+  const Expense = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: "452.23",
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Home Service",
+      amount: "50.00",
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={Expense[0].title}
+        amount={Expense[0].amount}
+        date={Expense[0].date}
+      />
     </div>
   );
 }
