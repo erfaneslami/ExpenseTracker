93e0742b270cfbc0970d1f9e36af91bf0a1e3a81
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-29 15:34:51 +0430

    hide the add form after submit

--- src/components/NewExpense/newExpense.js
+++ src/components/NewExpense/newExpense.js
@@ -11,6 +11,7 @@ const NewExpense = (props) => {
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    cancelEditingHandler();
   };
 
   const startEditingHandler = () => {
