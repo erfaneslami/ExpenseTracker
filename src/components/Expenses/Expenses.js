65aaff54cf0c5a13ee17089bfa4dcefb1b0ac653
erfan eslami <erfan.88.eslami@gmail.com>
2022-03-29 10:58:04 +0430

    refactor filter render method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,22 +15,24 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expenseContent = <p>no expenses Found !</p>;
+
+  if (filteredExpenses.length > 0)
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterChange={filterChangeHandler}
       />
-      <ul>
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </ul>
+      <ul>{expenseContent}</ul>
     </Card>
   );
 };
